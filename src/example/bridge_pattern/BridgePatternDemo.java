ef995ac5329ca0beda57d82eb9b7a14dd093fa65
asus <836585692@qq.com>
2022-06-08 08:58:12 +0800

    6.8 一天两个舒舒服服

---
+++ src/example/bridge_pattern/BridgePatternDemo.java
@@ -0,0 +1,8 @@
+package example.bridge_pattern;
+
+/**
+ * @Author ×£Ó¢Ì¨Õ¨ÓÍÌõ
+ * @Time : 2022/6/8 8:42
+ **/
+public class BridgePatternDemo {
+}
---
+++ src/example/filter_pattern/CriteriaPatternDemo.java
@@ -0,0 +1,8 @@
+package example.filter_pattern;
+
+/**
+ * @Author ×£Ó¢Ì¨Õ¨ÓÍÌõ
+ * @Time : 2022/6/8 8:57
+ **/
+public class CriteriaPatternDemo {
+}
