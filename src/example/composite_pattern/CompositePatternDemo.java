2768f514a75be3f1aabf5148445ef346c06c3653
asus <836585692@qq.com>
2022-06-09 08:56:12 +0800

    6.9 一天两个 舒服一整天

---
+++ src/example/composite_pattern/CompositePatternDemo.java
@@ -0,0 +1,8 @@
+package example.composite_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/9 8:43
+ **/
+public class CompositePatternDemo {
+}
---
+++ src/example/decorator_pattern/DecoratorPatternDemo.java
@@ -0,0 +1,8 @@
+package example.decorator_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/9 8:54
+ **/
+public class DecoratorPatternDemo {
+}
