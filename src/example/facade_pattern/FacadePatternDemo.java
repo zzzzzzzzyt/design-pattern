842c73bb6a99b8cd599dbba100d17a2276ffb819
asus <836585692@qq.com>
2022-06-10 08:58:07 +0800

    6.10 一天两个 舒服一整天 没多少天了

---
+++ src/example/facade_pattern/FacadePatternDemo.java
@@ -0,0 +1,10 @@
+package example.facade_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/10 8:44
+ * 外观模式  就是简化复杂的内部类的调用  把他都处理好了 给你一个接口就可以了
+ **/
+public class FacadePatternDemo {
+
+}
---
+++ src/example/flyweight_pattern/FlyweightPatternDemo.java
@@ -0,0 +1,9 @@
+package example.flyweight_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/10 8:45
+ * 享元模式  就是去重用
+ **/
+public class FlyweightPatternDemo {
+}
