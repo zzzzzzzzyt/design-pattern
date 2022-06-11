db4c6f4073370f572dd2ca3cffa5f927fd0dc395
asus <836585692@qq.com>
2022-06-11 09:13:54 +0800

    6.11 时间很少 Keep going

---
+++ src/example/chain_of_responsibility_pattern/ChainPatternDemo.java
@@ -0,0 +1,8 @@
+package example.chain_of_responsibility_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/11 9:06
+ **/
+public class ChainPatternDemo {
+}
---
+++ src/example/proxy_pattern/ProxyPatternDemo.java
@@ -0,0 +1,8 @@
+package example.proxy_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/11 8:28
+ **/
+public class ProxyPatternDemo {
+}
