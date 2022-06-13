d6a95fa36424b24eb911368ae2570680b12ff957
asus <836585692@qq.com>
2022-06-13 08:59:31 +0800

    6.13 神清气爽

---
+++ src/example/iterator_pattern/IteratorPatternDemo.java
@@ -0,0 +1,9 @@
+package example.iterator_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/13 8:39
+ **/
+public class IteratorPatternDemo {
+
+}
---
+++ src/example/mediator_pattern/MediatorPatternDemo.java
@@ -0,0 +1,8 @@
+package example.mediator_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/13 8:52
+ **/
+public class MediatorPatternDemo {
+}
