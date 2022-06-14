25a3b3e90426b9a60f815a9bd2fcedbe0cbf07d4
asus <836585692@qq.com>
2022-06-14 09:28:17 +0800

    6.14 加油一定要毕业

---
+++ src/example/memento_pattern/MementoPatternDemo.java
@@ -0,0 +1,8 @@
+package example.memento_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/14 9:15
+ **/
+public class MementoPatternDemo {
+}
---
+++ src/example/observer_pattern/ObserverPatternDemo.java
@@ -0,0 +1,8 @@
+package example.observer_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/14 9:26
+ **/
+public class ObserverPatternDemo {
+}
