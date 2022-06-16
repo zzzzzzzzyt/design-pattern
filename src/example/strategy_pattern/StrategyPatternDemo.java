4e6037777198678d6bb0decc38d4d201b06d8abe
asus <836585692@qq.com>
2022-06-16 08:44:45 +0800

    6.16 2/3完成

---
+++ src/example/strategy_pattern/StrategyPatternDemo.java
@@ -0,0 +1,8 @@
+package example.strategy_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/16 8:30
+ **/
+public class StrategyPatternDemo {
+}
---
+++ src/example/template_pattern/TemplatePatternDemo.java
@@ -0,0 +1,8 @@
+package example.template_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/16 8:42
+ **/
+public class TemplatePatternDemo {
+}
