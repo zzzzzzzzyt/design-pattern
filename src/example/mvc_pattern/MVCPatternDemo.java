ef1369bd1efbc24da3f542549e4de03734c30b4c
asus <836585692@qq.com>
2022-06-17 09:00:24 +0800

    6.17 继续并保持学习 兄弟

---
+++ src/example/mvc_pattern/MVCPatternDemo.java
@@ -0,0 +1,8 @@
+package example.mvc_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/17 8:59
+ **/
+public class MVCPatternDemo {
+}
---
+++ src/example/visitor_pattern/VisitorPatternDemo.java
@@ -0,0 +1,8 @@
+package example.visitor_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/17 8:58
+ **/
+public class VisitorPatternDemo {
+}
