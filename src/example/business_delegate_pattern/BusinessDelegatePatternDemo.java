ddb73e8fdf5a455352704199d8f16df2dfddba2e
asus <836585692@qq.com>
2022-06-18 08:41:57 +0800

    6.18 静待花开

---
+++ src/example/business_delegate_pattern/BusinessDelegatePatternDemo.java
@@ -0,0 +1,8 @@
+package example.business_delegate_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/18 8:39
+ **/
+public class BusinessDelegatePatternDemo {
+}
---
+++ src/example/composite_entity_pattern/CompositeEntityPatternDemo.java
@@ -0,0 +1,8 @@
+package example.composite_entity_pattern;
+
+/**
+ * @Author 祝英台炸油条
+ * @Time : 2022/6/18 8:40
+ **/
+public class CompositeEntityPatternDemo {
+}
